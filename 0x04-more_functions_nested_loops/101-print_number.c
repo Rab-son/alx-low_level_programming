4165f312ed445da16eb6b39e857d67f0297b320e
Rabson Sayenda <rabsonsayendajnr@gmail.com>
2022-06-10 01:00:06 +0200

    error fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,38 @@
-#include "main.h"
+#include "holberton.h"
 /**
-* print_number - that prints an integer
-*@n: print int
-* Return: Always 0.
+* print_number - print the number
+* @n: the character
+*
 */
+
 void print_number(int n)
 {
+	unsigned int sig, cont, num, dig, div, x;
+
+	cont = 1;
+	dig = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		sig = -n;
+	}
+	else
+	{
+		sig = n;
+	}
+	num = sig;
+	while (num > 9)
+	{
+		num = num / 10;
+		cont++;
+		dig = dig * 10;
+	}
+	for (x = 1; x <= cont; x++)
+	{
+	div = sig / dig;
+	sig = sig % dig;
+	dig = dig / 10;
+	_putchar (div + '0');
+	}
+}
