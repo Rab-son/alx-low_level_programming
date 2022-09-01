237f6c134df319c29c355034122c69444ea9a18d
Rab-son <rabsonsayendajnr@gmail.com>
2022-09-01 23:15:27 +0200

    comments

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,12 +1,14 @@
 #include "lists.h"
 
 /**
- * main - check the code
+ * add_dnodeint - adds a new node at the beginning
+ * of a dlistint_t list
  *
- * Return: Always EXIT_SUCCESS.
- * Author: AlX School
+ * @head: head of the list
+ * @n: value of the element
+ * Return: the address of the new element
+ * Author: Alx School
  */
-
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
